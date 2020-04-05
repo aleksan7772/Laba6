6d09a57efbeedd43bc4ba7ee43734fe9f58d0bbd
aleksan7772 <61800484+aleksan7772@users.noreply.github.com>
2020-04-05 17:41:16 +0300

    1111

--- src/Book.java
+++ src/Book.java
@@ -3,28 +3,66 @@ public class Book {
     private String name;
     private int[] numberOfPages = new int[50];
     private String[] chapterList = new String[5];
-
-    public int getNumberPages() {
-        return numberPages;
-    }
-
-    public void setNumberPages(int numberPages) {
-        numberPages = numberPages;
-    }
-
     private String category;
     private Boolean palette;
 
         {
-            chapterList[1] = "Введение";
-            chapterList[2] = "Основы военного дела";
-            chapterList[3] = "Военные стратегии";
-            chapterList[4] = "Виды боевых машин";
-            chapterList[5] = "Сражение на воде";
+            chapterList[0] = "Введение";
+            chapterList[1] = "Основы военного дела";
+            chapterList[2] = "Военные стратегии";
+            chapterList[3] = "Виды боевых машин";
+            chapterList[4] = "Сражение на воде";
 
         }
 
+    public static int getNumberPages() {
+        return numberPages;
     }
+
+    public static void setNumberPages(int numberPages) {
+        Book.numberPages = numberPages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int[] getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int[] numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String[] getChapterList() {
+        return chapterList;
+    }
+
+    public void setChapterList(String[] chapterList) {
+        this.chapterList = chapterList;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Boolean getPalette() {
+        return palette;
+    }
+
+    public void setPalette(Boolean palette) {
+        this.palette = palette;
+    }
+}
 
 
 
