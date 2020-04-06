f10025ada240e742de527cfcc1575ee42352cddc
bratchvv <bratchvv@gmail.com>
2020-04-06 20:16:17 +0300

    hotfix from 06_04_2020

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,28 @@
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
         Book book = new Book();
         book.setName("Военное дело");
+        book.setPalette(true);
+        // Use by default
+        System.out.println(Arrays.toString(book.getChapterList()));
+        System.out.println(Arrays.toString(book.getNumberOfPages()));
 
+        // Use manual values
+        String[] chapterList = new String[5];
+        chapterList[0] = "Введение";
+        chapterList[1] = "Основы военного дела";
+        chapterList[2] = "Военные стратегии";
+        chapterList[3] = "Виды боевых машин";
+        chapterList[4] = "Сражение на воде";
+        book.setChapterList(chapterList);
+        int[] numberOfPages = new int[1000];
+        for (int i = 0; i < numberOfPages.length; i++) {
+            numberOfPages[i] = i;
+        }
+        book.setNumberOfPages(numberOfPages);
+        System.out.println(Arrays.toString(book.getChapterList()));
+        System.out.println(Arrays.toString(book.getNumberOfPages()));
     }
 }
